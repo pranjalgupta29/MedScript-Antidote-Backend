0a709d51b98ecd505bfd5d83d27c68b8ba12bd18
Pranjal <pgupta3_be19@thapar.edu>
2021-10-10 17:31:14 +0530

    Project code refactored

---
+++ AntiDote-master/Hospital/imp.py
@@ -0,0 +1,14 @@
+class secrets: 
+    SECRET_KEY = '&d*1e%p*@3l&dn_zta@jsualjmia!*_1(=hyrf7a6yw$u5jz_!'
+    EMAIL_HOST_USER = 'noreply.antidote@gmail.com'
+    EMAIL_HOST_PASSWORD = 'seproject@69'
+
+
+
+
+
+
+
+
+
+
